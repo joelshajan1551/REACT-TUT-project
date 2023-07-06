48252d1e16c63ecea07f118864bb1330339aa04f
joelshajan1551 <112760045+joelshajan1551@users.noreply.github.com>
2023-07-06 12:48:48 +0530

    1 file modified

--- src/main.jsx
+++ src/main.jsx
@@ -1,38 +1,32 @@
-import React, { Children } from "react";
-import ReactDom from "react-dom/client";
-import "./index.css";
+import React, { Children } from 'react';
+import ReactDom from 'react-dom/client';
+import './index.css';
 
 const books = [
   {
-    author: "James clear",
-    title: " Atomic Habits",
-    img: "https://images-eu.ssl-images-amazon.com/images/I/91bYsX41DVL._AC_UL600_SR600,400_.jpg",
-    id:1,
+    author: 'James clear',
+    title: ' Atomic Habits',
+    img: 'https://images-eu.ssl-images-amazon.com/images/I/91bYsX41DVL._AC_UL600_SR600,400_.jpg',
+    id: 1,
   },
 
   {
-    author: " Morgan Housel ",
-    title: "The Psychology of Money",
-    img: "https://m.media-amazon.com/images/I/71g2ednj0JL._AC_UY218_.jpg",
+    author: ' Morgan Housel ',
+    title: 'The Psychology of Money',
+    img: 'https://m.media-amazon.com/images/I/71g2ednj0JL._AC_UY218_.jpg',
     id: 2,
   },
 ];
 
-
-
 const Booklist = () => {
   return (
-     <section className="booklist">
-    {books.map((book) => {
-      const {img,title,author,id} = book;
-      return <Book book={book} 
-      key={book.id}/>;
-         
-
-      
-    })}
-  </section>
-    );
+    <section className="booklist">
+      {books.map((book) => {
+        const { img, title, author, id } = book;
+        return <Book book={book} key={book.id} />;
+      })}
+    </section>
+  );
 };
 const Book = (props) => {
   const { author, title, img } = props;
@@ -46,6 +40,6 @@ const Book = (props) => {
   );
 };
 
-const root = ReactDom.createRoot(document.getElementById("root"));
+const root = ReactDom.createRoot(document.getElementById('root'));
 
 root.render(<Booklist />);
